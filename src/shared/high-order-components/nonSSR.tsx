60c55bc82ad8f073bf554161443d43602dc17839
Francisco Peña <francisco@ebi.ac.uk>
2025-04-28 11:00:31 +0100

    feat: remove unused components

--- src/shared/high-order-components/nonSSR.tsx
+++
@@ -1,8 +0,0 @@
-import { PropsWithChildren } from "react";
-import dynamic from "next/dynamic";
-
-const NonSSR = (props: PropsWithChildren) => (
-  <>{props.children}</>
-);
-
-export default dynamic(() => Promise.resolve(NonSSR), { ssr: false });
