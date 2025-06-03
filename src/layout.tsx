587d792c55c8a2fb763e1e1c1cd9afc9be131189
Francisco Peña <francisco@ebi.ac.uk>
2025-06-03 17:40:09 +0100

    chore: removed duplication of html body and header tags

--- src/layout.tsx
+++ src/layout.tsx
@@ -12,42 +12,19 @@ config.autoAddCss = false;
 
 export default function RootLayout() {
   return (
-    <html lang="en">
-    <head>
-      <title>International Mouse Phenotype Consortium</title>
-      <link rel="icon" href="/favicon.ico" />
-    </head>
-    <body>
-    <script>
-      {` // Google Tag Manager
-              (function(w, d, s, l, i) {
-                  w[l] = w[l] || [];
-                  w[l].push({
-                      'gtm.start': new Date().getTime(),
-                      event: 'gtm.js'
-                  });
-                  var f = d.getElementsByTagName(s)[0],
-                      j = d.createElement(s),
-                      dl = l != 'dataLayer' ? '&l=' + l : '';
-                  j.async = true;
-                  j.src =
-                      'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-                  f.parentNode.insertBefore(j, f);
-              })(window, document, 'script', 'dataLayer', 'GTM-NZPSPWR');`}
-    </script>
-    <script
-      type="text/javascript"
-      id="usercentrics-cmp"
-      src="https://web.cmp.usercentrics.eu/ui/loader.js"
-      data-settings-id="y7WXZ02Q6JSq6Q"
-      async
-    />
-    <Providers>
-      <Layout>
-        <Outlet />
-      </Layout>
-    </Providers>
-    </body>
-    </html>
+    <>
+      <script
+        type="text/javascript"
+        id="usercentrics-cmp"
+        src="https://web.cmp.usercentrics.eu/ui/loader.js"
+        data-settings-id="y7WXZ02Q6JSq6Q"
+        async
+      />
+      <Providers>
+        <Layout>
+          <Outlet />
+        </Layout>
+      </Providers>
+    </>
   );
 }
