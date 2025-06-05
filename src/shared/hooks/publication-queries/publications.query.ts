de5b0bb0ae9fa82f0b2b02b5cc0c209792a319c5
Francisco Peña <francisco@ebi.ac.uk>
2025-06-05 16:22:42 +0100

    feat: removed publications search index query

--- src/shared/hooks/publication-queries/publications.query.ts
+++ src/shared/hooks/publication-queries/publications.query.ts
@@ -1,7 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetchData } from "@/api-service";
 import { Publication } from "@/components/PublicationsList/types.ts";
-import lunr from "lunr";
 
 export const usePublicationsQuery = (onlyConsortiumPublications: boolean) => {
   return useQuery<Array<Publication>>({
@@ -12,22 +11,6 @@ export const usePublicationsQuery = (onlyConsortiumPublications: boolean) => {
         : "publications/all_publications.json";
       return fetchData(fileName);
     },
-    staleTime: Infinity,
-  });
-};
-
-export const usePublicationsSearchIndexQuery = (
-  onlyConsortiumPublications: boolean,
-) => {
-  return useQuery({
-    queryKey: ["publications", "search-index", onlyConsortiumPublications],
-    queryFn: () => {
-      let fileName = onlyConsortiumPublications
-        ? "publications/consortium_publications_index.json"
-        : "publications/all_publications_index.json";
-      return fetchData(fileName);
-    },
-    select: (data) => lunr.Index.load(data),
     staleTime: Infinity,
   });
 };
