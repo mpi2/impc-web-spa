0b59ed2d5a356be0c9615adf6a158f174b7afd49
Francisco Peña <francisco@ebi.ac.uk>
2025-06-05 16:23:03 +0100

    feat: add _id prop to Publication type

--- src/components/PublicationsList/types.ts
+++ src/components/PublicationsList/types.ts
@@ -1,4 +1,3 @@
-
 export type Publication = {
   title: string;
   authorString: string;
@@ -7,9 +6,15 @@ export type Publication = {
   journalTitle: string;
   abstractText: string;
   pmId: string;
-  alleles: Array<{mgiGeneAccessionId: string; alleleSymbol: string}>;
+  alleles: Array<{ mgiGeneAccessionId: string; alleleSymbol: string }>;
   meshHeadingList: Array<string>;
-  grantsList: Array<{ acronym: string; agency: string; grantId: string; orderIn: number }>;
+  grantsList: Array<{
+    acronym: string;
+    agency: string;
+    grantId: string;
+    orderIn: number;
+  }>;
   // TODO: update types after it becomes available from the backend
   doi?: string;
-}
+  _id: string;
+};
