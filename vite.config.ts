49737cb8acc279bff96ad3f61f9640529771f811
Francisco Peña <francisco@ebi.ac.uk>
2025-06-18 16:45:23 +0100

    feat: installed vite-plugin-pwa, set basic configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), VitePWA({ registerType: "autoUpdate" })],
   server: {
     proxy: {
       "/proxy": {
